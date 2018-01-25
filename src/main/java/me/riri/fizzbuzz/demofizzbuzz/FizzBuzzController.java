d7a87d233a872196b5bd662b14acd74b06c3bd54
rekikhaile <rekikhaile@gmail.com>
2018-01-25 14:42:35 -0500

    Initial commit of fizzbuzz for week1 friday challenge

---
+++ src/main/java/me/riri/fizzbuzz/demofizzbuzz/FizzBuzzController.java
@@ -0,0 +1,65 @@
+package me.riri.fizzbuzz.demofizzbuzz;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Scanner;
+
+
+@RestController
+public class FizzBuzzController {
+
+    @RequestMapping("/")
+    public String fizzBuzz(){
+        int first = 1;
+        int last = 100;
+        String fizzy = "";
+        for(int i=first; i<=last; i++){
+            if(i%3 != 0 && i%5!=0)
+                fizzy = fizzy + String.valueOf(i)+" ";
+            if(i%3 == 0 && i%5!=0)
+                fizzy = fizzy + " Fizz ";
+            if(i%5 == 0 && i%3!=0)
+                fizzy = fizzy +" Buzz ";
+            if(i%3 == 0 && i%5==0)
+                fizzy = fizzy +(" FizzBuzz ");
+            //else
+               // fizzy = fizzy + String.valueOf(i) +" ";
+
+        }
+        return fizzy;
+    }
+
+
+
+    @RequestMapping("/bonus")
+    public String fizzBuzzBonus(){
+        Scanner reader = new Scanner(System.in);
+        int first = 1, count7=0, count17=0,count23=0;
+        System.out.println("How many numbers? Enter an integer ");
+        int last = reader.nextInt();
+
+        String fizzy = "", notFizzy="";
+        for(int i=first; i<=last; i++){
+            if(i%3 != 0 && i%5!=0)
+                fizzy = fizzy + String.valueOf(i)+" ";
+            if(i%3 == 0 && i%5!=0)
+                fizzy = fizzy + " Fizz ";
+            if(i%5 == 0 && i%3!=0)
+                fizzy = fizzy +" Buzz ";
+            if(i%3 == 0 && i%5==0)
+                fizzy = fizzy +(" FizzBuzz ");
+
+            if(i%7 == 0)
+                count7++;
+            if(i%17 == 0)
+                count17++;
+            if(i%23 == 0)
+                count23++;
+
+        }
+
+        //return fizzy+"</br></br>"+notFizzy;
+        return fizzy+"</br></br>"+"The number of multiples of 7 are "+String.valueOf(count7)+"</br>The number of multiples of 17 are "+String.valueOf(count17)+"</br>The number of multiples of 23 are "+String.valueOf(count23);
+    }
+}
